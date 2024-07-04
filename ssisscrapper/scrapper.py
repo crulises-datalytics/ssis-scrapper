90eb0fedc46b7f71ea035dab22b157a2385e936f
LucianoArgolo11 <luciano.argolo@lovelytics.com>
2024-07-04 17:57:46 -0300

    Feature - Added iteration through each pipeline: - Getting Attributes for each 'sub' task inside the pipelines componentes - Avoid bringing the EventHandlers that were adding more tasks than there were (because it was also bringing conditions as tasks) - Added more columns to final csv file, so now we can track where does each task come from and what is it doing

--- ssisscrapper/scrapper.py
+++ ssisscrapper/scrapper.py
@@ -20,11 +20,47 @@ def extract_executable_type(obj, node=None, result=None):
             extract_executable_type(item, node, result)
     return result
 
+def extract_executable_typeV2(obj, result=None):
+    if result is None:
+        result = []
+    if isinstance(obj, dict):
+        if 'Attributes' in obj and '{www.microsoft.com/SqlServer/Dts}ExecutableType' in obj['Attributes'] and ".EventHandlers" not in obj['Attributes']['{www.microsoft.com/SqlServer/Dts}refId']:
+            if obj['Attributes']['{www.microsoft.com/SqlServer/Dts}ExecutableType'].lower() == 'microsoft.pipeline' and '{www.microsoft.com/SqlServer/Dts}ObjectData' in obj:
+                pipeline_data = obj['{www.microsoft.com/SqlServer/Dts}ObjectData'].get('pipeline', {})
+                components = pipeline_data.get('components', {})
+                for component in components.get('component', []):
+                    result.append({
+                        'RefId': obj['Attributes'].get('{www.microsoft.com/SqlServer/Dts}refId', ''),
+                        'ExecutableType': obj['Attributes']['{www.microsoft.com/SqlServer/Dts}ExecutableType'],
+                        'ObjectName': obj['Attributes'].get('{www.microsoft.com/SqlServer/Dts}ObjectName', ''),
+                        'componentClassID': component["Attributes"].get('componentClassID', ''),
+                        'contactInfo': component["Attributes"].get('contactInfo', ''),
+                        'description': component["Attributes"].get('description', ''),
+                        'name': component["Attributes"].get('name', '') 
+                    })
+            else:
+                result.append({
+                    'RefId': obj['Attributes'].get('{www.microsoft.com/SqlServer/Dts}refId', ''),
+                    'ExecutableType': obj['Attributes']['{www.microsoft.com/SqlServer/Dts}ExecutableType'],
+                    'ObjectName': obj['Attributes'].get('{www.microsoft.com/SqlServer/Dts}ObjectName', ''),
+                    'componentClassID': obj['Attributes'].get('componentClassID', ''),
+                    'contactInfo': obj['Attributes'].get('contactInfo', ''),
+                    'description': obj['Attributes'].get('description', ''),
+                    'name': obj['Attributes'].get('name', '') 
+                })
+        for value in obj.values():
+            extract_executable_typeV2(value, result)
+    elif isinstance(obj, list):
+        for item in obj:
+            extract_executable_typeV2(item, result)
+    return result
+
+
 if __name__ == '__main__':
     with open('out.json') as f:
         data = json.load(f)
 
-    executable_types = extract_executable_type(data)
+    executable_types = extract_executable_typeV2(data)
     #executable_types = list(set(executable_types))
 
     df = pd.DataFrame(executable_types)
