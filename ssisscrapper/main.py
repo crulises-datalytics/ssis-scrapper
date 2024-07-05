f6d5a71bb85dde993f23b5f08c8fa4660d6c63e1
LucianoArgolo11 <luciano.argolo@lovelytics.com>
2024-07-05 17:16:31 -0300

    Feature - Starting to analyze or automate analysis for SSIS packages: - Created SSISAnalyzer to analyzise - Created Statistics folder to save statistics and data there - Generated Doc for all the SSIS's classes

--- ssisscrapper/main.py
+++ ssisscrapper/main.py
@@ -1,6 +1,5 @@
 #%%
 from SSISModule import SSISMigrator, SSISDiscovery
-import shutil
 import json
 import os
 
@@ -9,22 +8,18 @@ if __name__ == '__main__':
     target_dir = r"C:\Users\luciano.argolo\ssis-scrapper\dtsx"
 
     discovery = SSISDiscovery(dir_path)
-    files = discovery.get_dtsx_files()
+    files = discovery.get_files()
     discovery.extract_dtsx_files(target_dir, files)
 
     migrator = SSISMigrator()
-    dir_path = r"C:\Users\luciano.argolo\ssis-scrapper\dtsx"
     file_paths = os.listdir(dir_path)
-
 
 
     for file_name in file_paths:
         file_path = dir_path + "\\" + file_name
         parsed_data = migrator.parse_xml_file(file_path)
-        #executable_types = migrator.extract_executable_type(data)
         df = migrator.get_df(parsed_data)
         
-        #{file_path.split(".")[0]}
         with open((dir_path + "\\" + file_name).replace('dtsx', 'json'), "w") as f:
             f.write(json.dumps(parsed_data, indent=4))
         print("Parsed Data is written out to file")
